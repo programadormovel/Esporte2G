e969cb164a8b50d41a5c8eebe205224b84bcd9a0
programadormovel <programadormovel@gmail.com>
2023-11-09 07:51:41 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Pessoa qualquer = new Pessoa();
+        qualquer.setNome("Rebeca Andrade");
+        System.out.println(qualquer.getNome());
+
+
+    }
+}
